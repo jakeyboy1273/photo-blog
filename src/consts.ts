751647bdd552366aa94f60bf2ddf4a2bfad5c3de
Jake Davies <jakedavies1273@gmail.com>
2026-03-20 17:14:49 +0000

    Fix Pages issues from `photo-blog` URL

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const REPO_NAME = '/photo-blog';
 export const SITE_TITLE = 'Jake\'s Jolly Jaunts';
 export const SITE_DESCRIPTION = 'Photos and stories from jakeyboy1273.';
