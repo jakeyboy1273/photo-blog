57427719bc668f5eeb54f6e21ccbfb07018e474a
jakeyboy1273 <jakedavies1273@gmail.com>
2026-09-18 11:27:08 +0100

    standardise page header formats

--- src/consts.ts
+++ src/consts.ts
@@ -3,3 +3,6 @@
 
 export const SITE_TITLE = "Jake's Jolly Jaunts";
 export const SITE_DESCRIPTION = "Photos and stories from jakeyboy1273.";
+export const STORIES_DESCRIPTION =
+  "Written accounts from trips, trails, and travels.";
+export const GALLERIES_DESCRIPTION = "Photo collections from around the world.";
